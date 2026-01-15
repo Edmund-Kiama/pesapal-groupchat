a6e60c7edec8a994956d23e92f0732ede01bb969
Edmund <edmundgachanja@gmail.com>
2026-01-15 12:45:39 +0300

    refactor: changes wait delay from 30s to 1min

--- backend/database/db.js
+++ backend/database/db.js
@@ -7,19 +7,19 @@ if (!DATABASE_URL) {
 
 export const sequelize = new Sequelize(DATABASE_URL, {
   dialect: "postgres",
-  logging: false, // Set to console.log if you want to see SQL queries
+  logging: false, 
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false, // Required for Railway public connections
+      rejectUnauthorized: false,
     },
-    // This helps prevent the connection from hanging
+   
     keepAlive: true,
   },
   pool: {
     max: 5,
     min: 0,
-    acquire: 60000, // Wait 60s for a connection before timing out
+    acquire: 60000, 
     idle: 10000,
   },
 });
