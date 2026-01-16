cbf78617f93eeff4de2933d7605f06203563b760
Edmund <edmundgachanja@gmail.com>
2026-01-16 12:02:13 +0300

    changes backend port

--- backend/app.js
+++ backend/app.js
@@ -22,9 +22,7 @@ const app = express();
 // CORS configuration
 app.use(
   cors({
-    origin:
-      process.env.FRONTEND_URL ||
-      "http://localhost:3001",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true,
   })
 );
